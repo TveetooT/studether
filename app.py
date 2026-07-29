fa69d5c8d9bedb24108de8d1a4f72fdaaf171161
TveetooT <danilaserbakov03@gmail.com>
2026-07-29 19:27:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,9 +103,9 @@ async def cmd_test(message: types.Message):
 async def handle_text(message: types.Message):
     text = message.text
     user_id = message.from_user.id
-    action = get_field(user_id, "action")
-    if (action == "firstname"):
-        set_string_field(user_id, "firstname", text)
+    action = await get_field(user_id, "action")     
+    if action == "firstname":
+        await set_string_field(user_id, "firstname", text)  
 
 
 # ---------- Функция установки вебхука (будет вызвана при старте) ----------
